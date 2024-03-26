4aeb7a41a9e149f870fbf3b9b58c8ad1b2a75ab1
Usuario <Usuario@CPD02-M04>
2024-03-26 11:26:39 -0300

    Adicionar arquivos de projeto.

---
+++ Consoleatividade/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consoleatividade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite 0 para pares ou 1 para ímpares: ");
+            int opcao = int.Parse(Console.ReadLine());
+
+            int cont = opcao;
+
+            while (cont <= 20) { 
+            
+                Console.WriteLine(cont);
+                cont += 2;
+
+            }
+                
+         
+
+         Console.ReadKey();
+            
+        }
+    }
+}
